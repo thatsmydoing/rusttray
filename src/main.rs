c8469ee2e98ad6f0ac31cbda22349c7b17871830
Thomas Dy <thatsmydoing@gmail.com>
2016-08-26 18:16:36 +0800

    Add signal handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate chan;
+extern crate chan_signal;
+
 fn main() {
+    let signal = chan_signal::notify(&[chan_signal::Signal::INT, chan_signal::Signal::TERM]);
+
     println!("Hello, world!");
+
+    loop {
+        chan_select!(
+            signal.recv() => {
+                break;
+            }
+        );
+    }
+
+    println!("Cleaning up...");
 }
